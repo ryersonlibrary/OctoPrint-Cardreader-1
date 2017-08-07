33493037beb3647f6545f72894a57aed45b5a82d
Kieran <kieran.ramnarine@ryerson.ca>
2017-08-07 09:37:32 +0000

    modified:   octoprint_CardReader/__init__.py

--- octoprint_CardReader/__init__.py
+++ octoprint_CardReader/__init__.py
@@ -13,13 +13,13 @@ class CardreaderPlugin(octoprint.plugin.StartupPlugin, octoprint.plugin.EventHan
 
 	def on_event(self, event, payload):
 		if event == "PrinterStateChanged":
-			if payload['state_string'] == 'Printing':
+			if payload['state_id'] == 'PRINTING':
 				if self.isPrinting == False:
 					self._printer.pause_print()
 					self.isPrinting = True
 				else :
 					self._printer.resume_print()
-			if payload['state_string'] == 'Operational':
+			if payload['state_string'] == 'OPERATIONAL':
 				self.isPrinting = False
 						
 __plugin_name__ = "Cardreader Plugin"
